0317577a258f85da40c5c2d739920fcd9072653b
somecoder123 <135623212+somecoder123@users.noreply.github.com>
2023-07-10 09:35:39 +0300

    Update db.ts

--- src/api/v1/config/db.ts
+++ src/api/v1/config/db.ts
@@ -1,1 +1,14 @@
-lll
+import mysql, { Pool } from "mysql2/promise";
+let Con: Pool;
+// eslint-disable-next-line no-useless-catch
+try {
+  Con = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+  });
+} catch (error) {
+  throw error;
+}
+export default Con;
