0b1a68403a152884001b248ff0483b3eb8865648
somecoder123 <135623212+somecoder123@users.noreply.github.com>
2023-07-10 09:41:23 +0300

    Create vendors.ts

---
+++ src/api/v1/routes/vendors.ts
@@ -0,0 +1,22 @@
+import authController from "../controllers/auth.controller";
+import AuthMiddleware from "../middleware/auth";
+import Vendors from "../controllers/vendors.controller.";
+
+import { Request, Response, Router, NextFunction } from "express";
+//all here are examples on how they should know they have access
+const Vendors_modules = Router();
+Vendors_modules.post(
+  "/",
+  AuthMiddleware.VerifyToken,
+  AuthMiddleware.VerifyHasAccess,
+  Vendors.create
+);
+
+Vendors_modules.get(
+  "/",
+  AuthMiddleware.VerifyToken,
+  AuthMiddleware.VerifyHasAccess,
+  Vendors.create
+);
+
+export default Vendors_modules;
